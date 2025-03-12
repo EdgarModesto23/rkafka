fc7eeaf2bf68f1e8c96d16267da9f16e1133d426
Edgar Modesto <edgarmodesto23@gmail.com>
2025-03-11 23:36:02 -0700

    codecrafters submit [skip ci]

--- src/protocol/schema/requests/apiversions.rs
+++ src/protocol/schema/requests/apiversions.rs
@@ -1,3 +1,7 @@
+use anyhow::Error;
+use serde::Deserialize;
+use std::{fs::File, io::BufReader, path::Path};
+
 use bytes::{BufMut, BytesMut};
 
 use crate::{
@@ -8,6 +12,37 @@ use crate::{
     },
     rpc::{decode::DecodeError, encode::Encode},
 };
+
+use super::is_version_supported;
+
+#[derive(Deserialize, Debug)]
+pub struct SupportedVersionsKey {
+    pub key: i16,
+    pub min: i16,
+    pub max: i16,
+}
+
+fn get_supported_versions_bytes<P: AsRef<Path>>(path: P) -> Result<BytesMut, Error> {
+    let f = File::open(path)?;
+    let reader = BufReader::new(f);
+    let mut data_bytes = BytesMut::new();
+
+    let data: Vec<SupportedVersionsKey> = serde_json::from_reader(reader)?;
+
+    let arr_size = data.len() as i8 + 1;
+
+    data_bytes.put_i8(arr_size);
+
+    for key in data.iter() {
+        println!("{key:?}");
+        data_bytes.extend_from_slice(&key.key.to_be_bytes()[..]);
+        data_bytes.extend_from_slice(&key.min.to_be_bytes()[..]);
+        data_bytes.extend_from_slice(&key.max.to_be_bytes()[..]);
+        //tag buffer
+        data_bytes.put_u8(0);
+    }
+    Ok(data_bytes)
+}
 
 pub struct ApiVersionRequest {
     pub base_request: RequestBase,
@@ -62,9 +97,43 @@ impl ApiVersionRequest {
 impl Respond for ApiVersionRequest {
     fn get_response(&self) -> Result<bytes::BytesMut, DecodeError> {
         let mut response = BytesMut::new();
-        response.put_slice(&[0, 0, 0, 0]);
+        let data = match get_supported_versions_bytes("supported_versions.json") {
+            Ok(supported_keys) => supported_keys,
+            Err(e) => {
+                return Err(DecodeError::InvalidBuffer(format!(
+                    "Error while decoding supported keys: {e:?}"
+                )))
+            }
+        };
+        let res_size = (4 + 2 + data.len() + 5) as i32;
+        let error: i16 = match is_version_supported(
+            "supported_versions.json",
+            self.base_request.api_key,
+            self.base_request.api_version,
+        ) {
+            Ok(value) => {
+                if value {
+                    0
+                } else {
+                    35
+                }
+            }
+            Err(e) => {
+                return Err(DecodeError::InvalidBuffer(format!(
+                    "Error while getting supported versions: {e:?}"
+                )))
+            }
+        };
+        println!("{res_size:?}");
+        res_size.encode(&mut response);
         self.base_request.correlation_id.encode(&mut response);
-        response.put_slice(&[0, 35]);
+        response.put_slice(&error.to_be_bytes());
+        response.put_slice(&data[..]);
+        //throttle ms
+        response.put_slice(&[0, 0, 0, 0]);
+        //tag buffer
+        response.put_u8(0);
+
         Ok(response)
     }
 }
--- src/protocol/schema/requests/mod.rs
+++ src/protocol/schema/requests/mod.rs
@@ -1,1 +1,135 @@
+use std::{fs::File, io::BufReader, path::Path};
+
+use anyhow::Error;
+use apiversions::SupportedVersionsKey;
+
 pub mod apiversions;
+
+/// Checks if a given version is supported for a specific key.
+///
+/// This function reads a JSON file (`supported_versions.json`) which contains a list
+/// of supported version ranges for various keys. It checks if the provided `key` and
+/// `version` fall within a valid supported version range.
+///
+/// # Arguments
+///
+/// * `key` - An `i16` representing the key for which the version is being checked.
+/// * `version` - An `i16` representing the version to be validated.
+///
+/// # Returns
+///
+/// * `Ok(true)` if the version is supported for the given key.
+/// * `Ok(false)` if the version is not supported for the given key.
+/// * `Err(Error)` in case of any errors reading the JSON file or parsing the data.
+///
+/// # Errors
+///
+/// This function may return an error if there is an issue opening the file, reading it,
+/// or parsing the JSON content. These errors are propagated using the `anyhow::Error` type.
+pub fn is_version_supported<P: AsRef<Path>>(
+    path: P,
+    key: i16,
+    version: i16,
+) -> Result<bool, Error> {
+    let f = File::open(path)?;
+    let reader = BufReader::new(f);
+
+    let data: Vec<SupportedVersionsKey> = serde_json::from_reader(reader)?;
+
+    Ok(data
+        .iter()
+        .filter(|val| val.key == key && (version >= val.min && version <= val.max))
+        .last()
+        .is_some())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+    use std::fs::{remove_file, File};
+    use std::io::Write;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct SupportedVersionsKey {
+        key: i16,
+        min: i16,
+        max: i16,
+    }
+
+    fn create_mock_supported_versions_json() {
+        let mock_data = vec![
+            SupportedVersionsKey {
+                key: 1,
+                min: 1,
+                max: 5,
+            },
+            SupportedVersionsKey {
+                key: 2,
+                min: 3,
+                max: 7,
+            },
+        ];
+
+        let file = File::create("supported_versions.json").expect("Failed to create mock file");
+        serde_json::to_writer(file, &mock_data).expect("Failed to write mock data");
+    }
+
+    #[test]
+    fn test_version_supported() {
+        create_mock_supported_versions_json();
+
+        let result = is_version_supported("supported_versions.json", 1, 3);
+        assert!(result.unwrap());
+
+        let result = is_version_supported("supported_versions.json", 1, 6);
+        assert!(!result.unwrap());
+        let result = is_version_supported("supported_versions.json", 2, 7);
+        assert!(result.unwrap());
+
+        let result = is_version_supported("supported_versions.json", 2, 2);
+        assert!(!result.unwrap());
+    }
+
+    #[test]
+    fn test_version_not_supported() {
+        create_mock_supported_versions_json();
+
+        let result = is_version_supported("supported_versions.json", 1, 6);
+        assert!(!result.unwrap());
+
+        let result = is_version_supported("supported_versions.json", 2, 8);
+        assert!(!result.unwrap());
+
+        let _ = remove_file("supported_versions.json");
+    }
+
+    #[test]
+    fn test_invalid_file() {
+        let result = is_version_supported("unexisting_file.csv", 1, 3);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_json_parse_error() {
+        let mut file =
+            File::create("supported_versions_marlformed.json").expect("Failed to create mock file");
+        file.write_all(b"invalid json")
+            .expect("Failed to write invalid data");
+
+        let result = is_version_supported("supported_versions_marlformed.json", 1, 3);
+        assert!(result.is_err());
+        let _ = remove_file("supported_versions_marlformed.json");
+    }
+
+    #[test]
+    fn test_empty_file() {
+        let file =
+            File::create("supported_versions_empty.json").expect("Failed to create mock file");
+        drop(file);
+
+        let result = is_version_supported("supported_versions_empty.json", 1, 3);
+        assert!(result.is_err());
+        let _ = remove_file("supported_versions_empty.json");
+    }
+}
